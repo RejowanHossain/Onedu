d79ee0da8d713195c33814ab1435eb429ffe094c
RejowanHossain <rezwan.hossain@outlook.com>
2021-04-03 15:54:53 +0600

    finished topbar and navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,13 +17,13 @@
             sticky menu
         ----------------------------------------------------*/
 
-        $(window).on("scroll", function() {
-            if ($(window).scrollTop()) {
-                $(".header").addClass("black");
-            } else {
-                $(".header").removeClass("black");
-            }
-        });
+        // $(window).on("scroll", function() {
+        //     if ($(window).scrollTop()) {
+        //         $(".header").addClass("black");
+        //     } else {
+        //         $(".header").removeClass("black");
+        //     }
+        // });
 
         /*---------------------------------------------------
             product carousel
