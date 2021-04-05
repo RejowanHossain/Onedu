45762d1f8d60bdf817fb6d557c9c2a147e59dfac
RejowanHossain <rezwan.hossain@outlook.com>
2021-04-05 19:46:09 +0600

    working on responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,10 +47,19 @@
             slidesToShow: 3,
             slidesToScroll: 1,
             responsive: [{
+                    breakpoint: 1060,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: false
+                    }
+                },
+                {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
                         infinite: true,
                         dots: false
                     }
@@ -99,6 +108,15 @@
             slidesToShow: 3,
             slidesToScroll: 1,
             responsive: [{
+                    breakpoint: 1060,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: false
+                    }
+                },
+                {
                     breakpoint: 1024,
                     settings: {
                         slidesToShow: 3,
